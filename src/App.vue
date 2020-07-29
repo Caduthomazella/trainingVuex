d19410b97e55695ccb831a9ecf082875804c8200
Carlos Eduardo Leal Thomazella <caduthomazella@hotmail.com>
2020-07-29 17:36:18 -0300

    Adicionado Vuex e Router no projeto e state para store

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/tasks">Tarefas</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
